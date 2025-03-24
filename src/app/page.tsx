4232c0f13da3cd55cc5c3ece084135afc47e7392
shakti-kc <shaktikc63@gmail.com>
2025-03-24 21:55:40 +0545

    built involve page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,7 @@
-import About from "./_sections/About";
-import Events from "./_sections/Events";
-import HeroSection from "./_sections/Hero";
-import JoinsUs from "./_sections/JoinsUs";
-import Newsletter from "./_sections/Newsletter";
-import News from "./_sections/News"
-import Partners from "./_sections/Partners";
-
 export default function Home() {
-  return(
+  return (
     <div className="homePageWrapper">
-      <HeroSection />
-      <About />
-      <Events />
-      <JoinsUs />
-      <Partners/>
-      <News/>
-      <Newsletter />
+      <h1>Home Page</h1>
     </div>
   );
 }
